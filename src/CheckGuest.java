9a8ec29c75c049a61c4f978f51502ac1e8317e19
Annagloria01 <annagloria05@gmail.com>
2025-09-11 17:10:50 +0200

    ciclo while

--- src/CheckGuest.java
+++ src/CheckGuest.java
@@ -4,32 +4,40 @@ import java.util.Scanner;
 public class CheckGuest {
 
     private static String[] nomiInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis, Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-   
+
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Inserisci il tuo nome utente");
-        String tuoNome= scanner.nextLine();
-
+        String tuoNome = scanner.nextLine();
 
         boolean found = false;
 
-        for (int i = 0; i < nomiInvitati.length ; i++) {
+        /* 
+        for (int i = 0; i < nomiInvitati.length; i++) {
             String currentElement = nomiInvitati[i];
-            if(tuoNome.equals(currentElement)){
-               found = true; 
+            if (tuoNome.equals(currentElement)) {
+                found = true;
             }
         }
+         */
 
-        if(found){
+         
+        int i = 0;
+        while (i < nomiInvitati.length) {
+            String currentElement = nomiInvitati[i];
+            if (tuoNome.equals(currentElement)) {
+                found = true;
+            }
+            i++;
+        }
+
+        if (found) {
             System.out.println("Puoi entrare");
-        } else{
+        } else {
             System.out.println("Non puoi entrare");
         }
 
-
     }
-    
-    
 
 }
